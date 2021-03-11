44e49ae662b80ae451e7c019f2876c699ec9f332
Michael Ippolito <michael.ippolito@gmail.com>
2021-03-10 21:29:22 -0600

    .

--- data607/project2/data607-1.py
+++ data607/project2/data607-1.py
@@ -111,13 +111,9 @@ for agentElement in agentArray:
 		agentAuthors = agentAuthors[0:-1]
 
 	# Output to main csv.
-	outLine = agentId + "," + agentName + "," + agentLink + "," + agentAgency + "," + agentGenres + "," + agentAuthors
+	outLine = agentId + ",\"" + agentName + "\",\"" + agentLink + "\",\"" + agentAgency + "\",\"" + agentGenres + "\",\"" + agentAuthors + "\""
 	outLine = remove_control_chars(outLine)						#remove non-printing characters
 	outLine = re.sub('&amp;', '&', outLine)						#replace ampersands
-
-	# Check for commas in any of the output fields, which would get in my kitchen.
-	if "," in agentId or "," in agentName or "," in agentLink or "," in agentAgency or "," in agentGenres or "," in agentAuthors:
-		print("Comma in one or more fields: " + outLine)
 
 	# Output to screen and file
 	print(outLine)
